50e5322ce01caf3ec8bd300550873366d6d1c512
sarukash218 <sarukash008@gmail.com>
2026-08-25 16:42:08 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!!!!!!!!!!!!!!!!!");
-        system.out.println("bye....");
+        System.out.println("bye....");
     }
 }
